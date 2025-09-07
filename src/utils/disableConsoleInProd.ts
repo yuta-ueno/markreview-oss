a4a5b599f0a1b24ccc66ea8a2dad16c941226714
yuta-ueno <yuta.ueno28@gmail.com>
2025-09-07 15:15:32 +0900

    chore: types for disableConsoleInProd; clear lint warnings

--- src/utils/disableConsoleInProd.ts
+++ src/utils/disableConsoleInProd.ts
@@ -1,18 +1,23 @@
 const noop = () => {}
 
-if (!(import.meta as any).env?.DEV) {
+type ImportMetaWithEnv = { env?: { DEV?: boolean } }
+type ConsoleLike = {
+  log: (...args: unknown[]) => void
+  info: (...args: unknown[]) => void
+  debug: (...args: unknown[]) => void
+  warn: (...args: unknown[]) => void
+}
+
+const isDev = ((import.meta as unknown as ImportMetaWithEnv).env?.DEV) === true
+
+if (!isDev) {
   try {
-    // Silence non-error logs in production builds
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ;(console as any).log = noop
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ;(console as any).info = noop
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ;(console as any).debug = noop
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ;(console as any).warn = noop
+    const c = console as unknown as ConsoleLike
+    c.log = noop
+    c.info = noop
+    c.debug = noop
+    c.warn = noop
   } catch {
     // no-op
   }
 }
-
